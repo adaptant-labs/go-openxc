234664d0b16ab3052e5acd984c7f42364de8b6e8
Paul Mundt <paul.mundt@adaptant.io>
2018-12-26 00:07:21 +0100

    Add additional VehicleState fields.

--- openxc.go
+++ openxc.go
@@ -26,13 +26,25 @@ type VehicleState struct {
 	HighBeamStatus        bool
 	WindshieldWiperStatus bool
 	BrakePedalStatus      bool
+	ParkingBrakeStatus    bool
 
 	DoorStatus       string
 	IgnitionStatus   string
 	TurnSignalStatus string
 
-	GearLevelPosition        string
+	GearLeverPosition        string
 	TransmissionGearPosition string
+
+	FuelLevel                float64
+	Latitude                 float64
+	Longitude                float64
+	AcceleratorPedalPosition float64
+	EngineSpeed              float64
+	VehicleSpeed             float64
+	FuelConsumedSinceRestart float64
+	Odometer                 float64
+	SteeringWheelAngle       float64
+	TorqueAtTransmission     float64
 }
 
 // VehicleMessage is the top-level wrapper for the OpenXC messaging format. Two
@@ -62,10 +74,36 @@ func VehicleMessageToState(state *VehicleState, msg VehicleMessage) {
 		state.BrakePedalStatus = msg.Value.(bool)
 	case "windshield_wiper_status":
 		state.WindshieldWiperStatus = msg.Value.(bool)
+	case "parking_brake_status":
+		state.ParkingBrakeStatus = msg.Value.(bool)
 	case "door_status":
 		state.DoorStatus = msg.Value.(string)
 	case "ignition_status":
 		state.IgnitionStatus = msg.Value.(string)
+	case "gear_lever_position":
+		state.GearLeverPosition = msg.Value.(string)
+	case "transmission_gear_position":
+		state.TransmissionGearPosition = msg.Value.(string)
+	case "fuel_level":
+		state.FuelLevel = msg.Value.(float64)
+	case "latitude":
+		state.Latitude = msg.Value.(float64)
+	case "longitude":
+		state.Longitude = msg.Value.(float64)
+	case "accelerator_pedal_position":
+		state.AcceleratorPedalPosition = msg.Value.(float64)
+	case "engine_speed":
+		state.EngineSpeed = msg.Value.(float64)
+	case "vehicle_speed":
+		state.VehicleSpeed = msg.Value.(float64)
+	case "fuel_consumed_since_restart":
+		state.FuelConsumedSinceRestart = msg.Value.(float64)
+	case "odometer":
+		state.Odometer = msg.Value.(float64)
+	case "steering_wheel_angle":
+		state.SteeringWheelAngle = msg.Value.(float64)
+	case "torque_at_transmission":
+		state.TorqueAtTransmission = msg.Value.(float64)
 	default:
 		fmt.Println("unhandled message for type", msg.Name)
 	}
